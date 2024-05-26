ec4b7aa13e5dca41404b26c966476195b47a4875
BigolJude <judebarnett2018@gmail.com>
2024-05-26 18:11:37 +0100

    Chog - Basic movement working

--- Objects/Chog/chog.cs
+++ Objects/Chog/chog.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
 
-public partial class chog : Node2D
+public partial class chog : CharacterBody2D
 {
 	private const string WALK_LEFT = "walk_left";
 	private const string WALK_RIGHT = "walk_right";
 	private const string SPRINT_LEFT = "sprint_left";
 	private const string SPRINT_RIGHT = "sprint_right";
+	private const string GRAVITY_SETTING_LOCATION = "physics/2d/default_gravity";
 	private const string JUMP = "jump";
+	
+	public float Gravity = ProjectSettings.GetSetting(GRAVITY_SETTING_LOCATION).AsSingle();
+	
 	
 	[Export]
 	public int Speed { get; set;} = 400;
+	
+	[Export]
+	public int JumpSpeed { get; set; } = -725;
+	
+	[Export]
+	public int JumpDuration {get; set;} = 10;
 	
 	[Signal]
 	public delegate void HitEventHandler();
@@ -26,30 +36,31 @@ public partial class chog : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 velocity = Vector2.Zero;
+		Vector2 velocity = Velocity;
+		velocity.Y += Gravity * (float)delta;
+		velocity.X = 0;
 		
 		if(Input.IsActionPressed(WALK_LEFT) || Input.IsActionPressed(SPRINT_LEFT))
 		{
-			velocity.X -= 1;	
+			velocity.X = -Speed;	
 		} 
 		if(Input.IsActionPressed(WALK_RIGHT) || Input.IsActionPressed(SPRINT_RIGHT))
 		{
-			velocity.X += 1;	
+			velocity.X = Speed;	
+		}
+		
+		if(Input.IsActionJustPressed(JUMP) && IsOnFloor())
+		{
+			GD.Print("IsOnFloor called");
+			velocity.Y = JumpSpeed;
 		}
 		
 		AnimatedSprite2D animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		animatedSprite.FlipH = velocity.X < 0;
 		
-		if(velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-		}
-		
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
+		Velocity = velocity;
+		MoveAndSlide();
 		
 	}
+	
 }
